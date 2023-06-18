cf405a5ebc21820ecc1481f7015fb6117090fdc1
Thành Trung <trungpspy@gmail.com>
2023-06-18 12:31:16 +0700

    update

--- Sort.h
+++ Sort.h
@@ -1,3 +1,5 @@
+#include <iostream>
+using namespace std;
 void Swap(int *a, int *b)
 {
     int tmp = *a;
@@ -15,5 +17,28 @@ void InsertionSort(int *a, int n)
                 Swap(&a[i], &a[j]);
             }
         }
+    }
+}
+
+void SelectionSort(int *a, int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int tmp = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (a[tmp] > a[j])
+            {
+                tmp = j;
+            }
+        }
+        Swap(&a[i], &a[tmp]);
+    }
+}
+void print(int *a, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << "  ";
     }
 }
--- sourcecode.cpp
+++ sourcecode.cpp
@@ -1,32 +1,4 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <math.h>
-#include <stdio.h>
-using namespace std;
-
-void print(int *a, int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        cout << a[i] << "  ";
-    }
-}
-void SelectionSort(int *a, int n)
-{
-    for (int i = 0; i < n - 1; i++)
-    {
-        int tmp = i;
-        for (int j = i + 1; j < n; j++)
-        {
-            if (a[tmp] > a[j])
-            {
-                tmp = j;
-            }
-        }
-        Swap(&a[i], &a[tmp]);
-    }
-}
+#include "Sort.h"
 int main()
 {
     int a[]{2, 3, 4, 6, 8, 4, 2, 1, 8};
